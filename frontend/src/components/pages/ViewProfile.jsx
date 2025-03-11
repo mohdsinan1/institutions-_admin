3cc227f61a3a2913530e2daebbc60464891ad6d9
Mohammed Adhil <mohammedadhilkm68@gmail.com>
2025-03-11 17:13:38 +0530

    sample-commit

--- frontend/src/components/pages/ViewProfile.jsx
+++ frontend/src/components/pages/ViewProfile.jsx
@@ -28,7 +28,7 @@ function ViewProfile() {
       <div className="container-fluid  mt-5 h-75">
       <div className="emp-profile bg-white p-5 shadow-lg rounded h-75
      ">
-       <form className="">
+     
   <div className="row align-items-center g-4">
     {/* Profile Image */}
     <div className="col-md-4 text-center">
@@ -92,6 +92,7 @@ function ViewProfile() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
