1e1684a7094d5065d12b13c836338c29b5a7c9eb
Mohammed Adhil <mohammedadhilkm68@gmail.com>
2025-03-12 11:24:28 +0530

    backend

---
+++ backend/routes/route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { Login } = require("../controllers/AuthController");
+
+const routes =express.Router()
+
+
+routes.get('/', )
+
+
+
+
+
+
+module.exports = routes;
